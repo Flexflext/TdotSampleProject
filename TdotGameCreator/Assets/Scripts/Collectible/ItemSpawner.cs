b78b7dac404d5731c759ac94b9887316ad2c4648
Flexflext <felix.hereth.fh@googlemail.com>
2023-01-02 20:36:20 +0100

    zjtr

--- TdotGameCreator/Assets/Scripts/Collectible/ItemSpawner.cs
+++ TdotGameCreator/Assets/Scripts/Collectible/ItemSpawner.cs
@@ -5,34 +5,47 @@ using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
+    
     [SerializeField] private AnimationCurve itemCurve;
+    [Min(1)]
     [SerializeField] private int amountOfItems;
+    [Min(1)]
+    [SerializeField] private int rowAmount;
+    [SerializeField] private float rowSpacing;
+    [SerializeField] private float collumSpacing;
     [SerializeField] private GameObject itemToSpawn;
 
-    private Vector2[] positions;
+    private Vector2[,] positions;
 
     private void GeneratePositions()
     {
-        positions = new Vector2[amountOfItems];
+        positions = new Vector2[amountOfItems, rowAmount];
 
         float curPosition = itemCurve[0].time;
 
         float addpercent = Mathf.Abs(itemCurve[0].time - itemCurve[itemCurve.length-1].time) / amountOfItems;
 
-        for (int i = 0; i < positions.Length; i++)
-        {
-            positions[i] = transform.position + new Vector3(curPosition, itemCurve.Evaluate(curPosition), 0);
-            curPosition += addpercent;
-        }
+        
+            for (int i = 0; i < amountOfItems; i++)
+            {
+                for (int j = 0; j < rowAmount; j++)
+                {
+                    positions[i, j] = transform.position + new Vector3(curPosition, itemCurve.Evaluate(curPosition) * collumSpacing, 0) + ((Vector3.up * rowSpacing) * j);
+                }
+                curPosition += addpercent;
+            }
     }
     
     
      private void OnDrawGizmos()
     {
         GeneratePositions();
-        for (int i = 0; i < positions.Length; i++)
+        for (int i = 0; i < amountOfItems; i++)
         {
-            Gizmos.DrawSphere(positions[i], 0.1f);
+            for (int j = 0; j < rowAmount; j++)
+            {
+                Gizmos.DrawSphere(positions[i, j], 0.1f);
+            }
         }
     }
 }
